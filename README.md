7966a49f3da49ed8a2d2471823b740e0da1e0ba0
madhu <madhubabugopisetti27@gmail.com>
2025-12-26 15:34:19 +0530

    GOAL 2: Move model in Gazebo and rviz with ROS

--- README.md
+++ README.md
@@ -16,6 +16,8 @@ colcon build --symlink-install
 source install/setup.bash
 ```
 
+# GOAL 1: Render model in Gazebo
+
 ## STEP 1: Creating a workspace
 ```
 mkdir -p ~/ros2_autodesk_ws/src
@@ -58,3 +60,71 @@ ros2 pkg create robot_description --build-type ament_cmake
 - ros2 launch robot_description world.launch.py<br />
 - ros2 launch robot_description gazebo.launch.py<br />
 - ros2 launch robot_description rviz.launch.py<br />
+
+# GOAL 2: Move model in Gazebo and rviz
+**Major Changes are in this Goal**
+
+## STEP 1: 
+- Add a Base into xacro file(2D ground reference)
+```
+<!-- Base footprint (planar frame) -->
+	<joint name="base_footprint_joint" type="fixed">
+		<origin xyz="0 0 0.05" rpy="0 0 0" />
+		<parent link="base_footprint" />
+		<child link="base_link" />
+	</joint>
+```
+- Add controller plugin and for friction in same file at last before </robot> tag
+```
+    <gazebo>
+		<plugin filename="gz-sim-diff-drive-system"
+			name="gz::sim::systems::DiffDrive">
+			<left_joint>wheel_left_joint</left_joint>
+			<right_joint>wheel_right_joint</right_joint>
+			<wheel_separation>0.20</wheel_separation>
+			<wheel_radius>0.05</wheel_radius>
+			<topic>/cmd_vel</topic>
+			<odom_topic>/odom</odom_topic>
+			<tf_topic>/tf</tf_topic>
+			<frame_id>odom</frame_id>
+			<child_frame_id>base_link</child_frame_id>
+			<odom_publish_frequency>30</odom_publish_frequency>
+		</plugin>
+		<plugin filename="gz-sim-joint-state-publisher-system"
+			name="gz::sim::systems::JointStatePublisher">
+			<topic>joint_states</topic>
+			<joint_name>wheel_left_joint</joint_name>
+			<joint_name>wheel_right_joint</joint_name>
+		</plugin>
+	</gazebo>
+	<gazebo reference="left_wheel_1">
+		<mu1>1.0</mu1>
+		<mu2>1.0</mu2>
+	</gazebo>
+	<gazebo reference="right_wheel_1">
+		<mu1>1.0</mu1>
+		<mu2>1.0</mu2>
+	</gazebo>
+	<gazebo reference="caster_1">
+		<mu1>0.0</mu1>
+		<mu2>0.0</mu2>
+	</gazebo>
+```
+## STEP 2: Test movement now
+- [BUILD](#build)<br />
+- Terminal 1: ros2 launc robot_description gazebo.launch.py<br />
+- Terminal 2: ros2 topic pub /cmd_vel geometry_msgs/Twist "{linear: {x: 0.3}, angular: {z: 0.5}}"<br />
+- Model should rotate
+
+## STEP 3: yaml, rviz changes (refer to github files)
+- Create a new file ros2_controllers.yaml inside config folder
+- Make changes in ros_gz_bridge.yaml 
+- display.rviz -> change Fixed Frame: base_link to odom
+
+## STEP 4: Synmc Gazebo and RVIZ with ROS2
+- First install teleop to control the model ```sudo apt install ros-jazzy-teleop-twist-keyboard```<br />
+- Create a file in launch folder(refer file for changes)<br />
+- [BUILD](#build)
+- Terminal 1: ros2 launc robot_description sim_rviz.launch.py<br />
+- Terminal 2: ros2 run teleop_twist_keyboard teleop_twist_keyboard<br />
+- You should be able to type commands and model will be moving
---
+++ src/robot_description/launch/sim_rviz.launch.py
@@ -0,0 +1,105 @@
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from os.path import join
+
+
+def generate_launch_description():
+
+    pkg_robot = get_package_share_directory('robot_description')
+    pkg_gz = get_package_share_directory('ros_gz_sim')
+
+    # --- Robot description ---
+    xacro_file = os.path.join(pkg_robot, 'urdf', 'robot_description.xacro')
+    robot_description_xml = xacro.process_file(xacro_file).toxml()
+    robot_description = {'robot_description': robot_description_xml}
+
+    # --- Robot State Publisher (TF) ---
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[robot_description, {'use_sim_time': True}],
+        output='screen'
+    )
+
+    # --- Gazebo ---
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            join(pkg_gz, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={
+            'gz_args': '-r -v 4 empty.sdf'
+        }.items()
+    )
+
+    # --- Spawn robot ---
+    spawn_robot = TimerAction(
+        period=3.0,
+        actions=[
+            Node(
+                package='ros_gz_sim',
+                executable='create',
+                arguments=[
+                    '-topic', '/robot_description',
+                    '-name', 'robot_description',
+                    '-z', '0.32'
+                ],
+                output='screen'
+            )
+        ]
+    )
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[
+            robot_description,
+            os.path.join(pkg_robot, "config", "ros2_controllers.yaml"),
+            {"use_sim_time": True}
+        ],
+        output="screen"
+    )
+
+    joint_state_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_drive_controller"],
+    )
+
+    # --- ROS ↔ Gazebo bridge ---
+    ros_gz_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        parameters=[
+            {'config_file': os.path.join(pkg_robot, 'config', 'ros_gz_bridge_gazebo.yaml')}
+        ],
+        output='screen'
+    )
+
+    # --- RViz ---
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=[
+            '-d', os.path.join(pkg_robot, 'config', 'display.rviz')
+        ],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        gazebo,
+        robot_state_publisher,
+        spawn_robot,
+        ros_gz_bridge,
+        rviz
+    ])
